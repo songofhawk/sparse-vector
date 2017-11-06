c709dd7717de005a830ab154d0993080fe8b75bd
songofhawk@gmail.com <songofhawk@gmail.com>
2017-11-06 21:44:39 +0800

    add english comment

--- src/com/nuanxinli/ai/vector/SparseVector.java
+++ src/com/nuanxinli/ai/vector/SparseVector.java
@@ -13,30 +13,35 @@ import org.apache.log4j.Logger;
 
 /**
  * 稀疏向量
- * @author Administrator
- * 宋辉
- *
+ * Sparse Vector
+ * @author 宋辉(Song Hui)
+ * 
  */
 public class SparseVector {
 
 	// 双值计算器回调方法,用于在合并向量的时候, 让外部调用指定如何合并
+	//a callback function, to calculate two values when mergeing vectors
 	@FunctionalInterface
 	public interface DualValueComputer {
-		float compute(Float coordValue1, Float coordValue2);
+		float calculate(Float divValue1, Float divValue2);
 	}
 	
 	//用一个map来保存向量各个维度的值(仅不为0的维度才保存)
+	//a map to store values for all divisions, only none-zero value will be stored
 	private Map<String, Float> divMap = new HashMap<>();
 	//长度的平方(这是一个缓存, 由计算方法squareOfLength生成,一旦生成就记录下来,以后不再计算了, 除非向量改变)
+	//the square of length on this vector, which is a cache value 
 	private Float lengthSquareCache;
 
 	//日志记录
+	//for logging
 	private static Logger logger = Logger.getLogger(SparseVector.class);
 
 	/**
 	 * 初始化向量
-	 * @param divNames 字符数组存储的维度名称
-	 * @param divValues 浮点数组存储的维度值
+	 * constructor with 2 parameters
+	 * @param divNames 字符数组存储的维度名称 - division names in string array 
+	 * @param divValues 浮点数组存储的维度值 - division values in float array
 	 */
 	public SparseVector(String[] divNames, float[] divValues)
 	{
@@ -49,8 +54,10 @@ public class SparseVector {
 	
 	/**
 	 * 初始化向量
-	 * @param divNames 字符数组存储的维度名称
-	 * 所有的穿入名称的维度值被初始化为1
+	 * constructor with 1 parameter
+	 * @param divNames 字符数组存储的维度名称 - division names in string array 
+	 * 所有的传入名称的维度值被初始化为1
+	 * all division values will be initialized to 1 in divNames
 	 */
 	public SparseVector(String[] divNames)
 	{
@@ -61,6 +68,7 @@ public class SparseVector {
 	
 	/**
 	 * 初始化向量
+	 * constructor with no parameter
 	 * 缺省不存在任何维度(相当于在任意维度上的值为0)
 	 */
 	public SparseVector() {
@@ -69,8 +77,9 @@ public class SparseVector {
 	
 	/**
 	 * 设置某个特定维度的值
-	 * @param divName 维度名称
-	 * @param divValue 维度值
+	 * set value of specified division 
+	 * @param divName 维度名称 - division name
+	 * @param divValue 维度值 - division value
 	 */
 	public void setDiv(String divName, float divValue)
 	{
@@ -82,18 +91,21 @@ public class SparseVector {
 
 	/**
 	 * 获取某个特定维度的值 
-	 * @param coordName 维度名称
-	 * @return 维度值
+	 * get value of specified division
+	 * @param divName 维度名称 - division name 
+	 * @return 维度值 - division value
 	 */
-	public Float getCoordValue(String coordName)
+	public Float getCoordValue(String divName)
 	{
-		return divMap.get(coordName);
+		return divMap.get(divName);
 	}
 	
 	/**
 	 * 本向量与另一个向量相加，返回新向量
-	 * @param vector 加向量
-	 * @return 两者之和组成的新向量
+	 * return a new vector, which equals this vector plus another one
+	 * as the count of divisions is not certain, any two vectors can be calculated by this function
+	 * @param vector 加向量 - another vector
+	 * @return 两者之和组成的新向量 - sum vector
 	 */
 	public SparseVector plus(SparseVector vector)
 	{
@@ -102,7 +114,9 @@ public class SparseVector {
 	
 	/**
 	 * 本向量与另一个向量相加，并把结果保存到自身
-	 * @param vector 加向量
+	 * plus another vector to this vector, this vector will equal the sum vector after function called.
+	 * as the count of divisions is not certain, any two vectors can be calculated by this function
+	 * @param vector 加向量 - another vector
 	 */
 	public void plusSelf(SparseVector vector)
 	{
@@ -111,8 +125,10 @@ public class SparseVector {
 	
 	/**
 	 * 本向量与另一个向量相减，返回新向量
-	 * @param vector 减向量
-	 * @return 两者之差组成的新向量
+	 * return a new vector, which equals this vector minus another one
+	 * as the count of divisions is not certain, any two vectors can be calculated by this function
+	 * @param vector 减向量 - another vector
+	 * @return 两者之差组成的新向量 - difference vector
 	 */
 	public SparseVector minus(SparseVector vector)
 	{
@@ -121,7 +137,9 @@ public class SparseVector {
 	
 	/**
 	 * 本向量与另一个向量相减，结果保存到自身
-	 * @param vector 减向量
+	 * this vector minus another one, this vector will equal the difference vector after function called.
+	 * as the count of divisions is not certain, any two vectors can be calculated by this function
+	 * @param vector 减向量 - another vector
 	 */
 	public void minusSelf(SparseVector vector)
 	{
@@ -130,8 +148,9 @@ public class SparseVector {
 	
 	/**
 	 * 本向量乘以一个常数，返回新向量
-	 * @param factor 乘数
-	 * @return 两者之积组成的新向量
+	 * return a new vector, which equals this vector multiply a constant
+	 * @param factor 乘数 - the constant
+	 * @return 两者之积组成的新向量 - the result vector
 	 */
 	public SparseVector multiply(float factor)
 	{
@@ -144,7 +163,8 @@ public class SparseVector {
 	
 	/**
 	 * 本向量乘以一个常数，结果保存到自身
-	 * @param factor 乘数
+	 * this vector multiply a constant, this vector will equal the product vector after function called.
+	 * @param factor 乘数 - the constant
 	 */
 	public void multiplySelf(float factor)
 	{
@@ -155,8 +175,9 @@ public class SparseVector {
 	
 	/**
 	 * 本向量除以一个常数，返回新向量
-	 * @param factor 除数
-	 * @return 两者之商组成的新向量
+	 * return a new vector, which equals this vector divide a constant
+	 * @param divisor 除数 - divisor
+	 * @return 两者之商组成的新向量 - the quotient vector
 	 */
 	public SparseVector divide(float divisor)
 	{
@@ -169,7 +190,8 @@ public class SparseVector {
 	
 	/**
 	 * 本向量除以一个常数，结果保存到自身
-	 * @param factor 除数
+	 * this vector divide by a constant, this vector will equal the quotient vector after function called.
+	 * @param factor 除数 - divisor
 	 */
 	public void divideSelf(float divisor)
 	{
@@ -180,8 +202,9 @@ public class SparseVector {
 
 	/**
 	 * 获取指定向量(点)集合的图心(几何中心)
-	 * @param vectors 指定向量集合
-	 * @return 几何中心向量
+	 * Get the centroid of a collection of vectors, also called as geometric center
+	 * @param vectors 指定向量集合 - the collection of vectors
+	 * @return 几何中心向量 - the centroid
 	 */
 	public static SparseVector getCentroid(Collection<SparseVector> vectors)
 	{
@@ -191,50 +214,60 @@ public class SparseVector {
 	}
 	
 	/**
-	 * 把当前向量和指定向量合并为一个新向量
-	 * @param vector 指定向量
-	 * @param computeFunc 合并算法，用于表达两个具体的向量维度合并时应该怎样计算
+	 * 把本向量和指定向量合并为一个新向量； 如果两者有相同的维度，它们的值会合并，合并方式取决于calculateFunc参数；如果是不同的维度，会直接被复制到结果中
+	 * merge this vector with another one, and return the result as a new vector.
+	 * the result will have union divisions of these 2 vectors. The same divisions will be merged, merging method depends on calculateFunc. 
+	 * the different divisions will be copied into result directly.    
+	 * @param vector 指定向量 - another vector
+	 * @param calculateFunc 合并算法，用于表达两个具体的向量维度合并时应该怎样计算 - merging method, which is a call back function to handle values from 2 vectors
 	 */
-	public SparseVector mergeVector(SparseVector vector, DualValueComputer computeFunc){
+	public SparseVector mergeVector(SparseVector vector, DualValueComputer calculateFunc){
 		SparseVector newVector = new SparseVector();
-		newVector.mergeVectorSelf(this, computeFunc);
-		newVector.mergeVectorSelf(vector, computeFunc);
+		newVector.mergeVectorSelf(this, calculateFunc);
+		newVector.mergeVectorSelf(vector, calculateFunc);
 		return newVector;
 	}
 	
 	/**
-	 * 把指定向量合并到当前向量
-	 * @param vector 指定向量
-	 * @param computeFunc 合并算法，用于表达两个具体的向量维度合并时应该怎样计算
+	 * 把指定向量合并到本向量； 如果两者有相同的维度，它们的值会合并，合并方式取决于calculateFunc参数；如果是不同的维度，会直接被复制到本向量中
+	 * merge another vector into this one.
+	 * the result will have union divisions of these 2 vectors. The same divisions will be merged, merging method depends on calculateFunc. 
+	 * the different divisions will be copied into this one directly.    
+	 * @param vector 指定向量 - another vector
+	 * @param calculateFunc 合并算法，用于表达两个具体的向量维度合并时应该怎样计算 - merging method, which is a call back function to handle values from 2 vectors
 	 */
-	private void mergeVectorSelf(SparseVector vector, DualValueComputer computeFunc){
+	private void mergeVectorSelf(SparseVector vector, DualValueComputer calculateFunc){
 		for (Map.Entry<String,Float> entry : vector.divMap.entrySet()){
 			String coordName = entry.getKey();
 			Float coordValue = entry.getValue();
 			Float originValue = this.getCoordValue(coordName);
-			this.setDiv(coordName, (originValue==null)? coordValue: computeFunc.compute(originValue,coordValue));
+			this.setDiv(coordName, (originValue==null)? coordValue: calculateFunc.calculate(originValue,coordValue));
 		}
 	}
 	
 	/**
 	 * 把一组向量集合合并为一个新的向量
+	 * merge a set of vectors to one vector.
+	 * the result will have union divisions of all vectors in the set. The same divisions will be merged, merging method depends on calculateFunc. 
+	 * the different divisions will be copied to the result.    
 	 * @param vectors 向量集合
-	 * @param computeFunc 合并算法，用于表达两个具体的向量维度合并时应该怎样计算
+	 * @param calculateFunc 合并算法，用于表达两个具体的向量维度合并时应该怎样计算
 	 * @return
 	 */
-	private static SparseVector mergeVectors(Collection<SparseVector> vectors, DualValueComputer computeFunc)
+	private static SparseVector mergeVectors(Collection<SparseVector> vectors, DualValueComputer calculateFunc)
 	{
 		SparseVector newVector = new SparseVector();
 		for (SparseVector vector : vectors){
-			newVector.mergeVectorSelf(vector, computeFunc);
+			newVector.mergeVectorSelf(vector, calculateFunc);
 		}
 		return newVector;
 	}
 	
 	/**
 	 * 计算本向量与指定向量的点积(标量积)
-	 * @param vector 指定向量
-	 * @return 点积值
+	 * calculate "dot product/scalar product" of this vector and another vector
+	 * @param vector 指定向量 - another vector
+	 * @return 点积值 - dot product value
 	 */
 	public float dotProduct(SparseVector vector)
 	{
@@ -255,24 +288,44 @@ public class SparseVector {
 	/**
 	 * 求指定向量与本向量的距离平方, 其实也就是各个维度坐标差的平方和
 	 * 因为求距离的话,还要把最后的平方和再开方一次, 如果只是用于比较大小, 得到距离平方就够了
-	 * @param vector2
-	 * @return 距离的平方
+	 * Get the square of distance between this vector and another one. 
+	 * @param vector 指定向量(如果为空,表示0向量,则返回本向量的长度) - another vector(if is null, than return the square of length of this vector)
+	 * @return 距离的平方 - the square of distance
 	 */
-	public float squareOfDistance(SparseVector vector2)
+	public float squareOfDistance(SparseVector vector)
 	{
-		if (vector2==null){
+		if (vector==null){
 			return this.squareOfLength(false);
 		}
-		SparseVector newVector = minus(vector2);
+		SparseVector newVector = minus(vector);
 		return newVector.squareOfLength(false);
 	}
 	
 	/**
+	 * 求指定向量与本向量的距离
+	 * Get the distance between this vector and another one. 
+	 * @param  指定向量(如果为空,表示0向量,则返回本向量的长度) - another vector(if is null, then return the length of this vector)
+	 * @return 距离 - the distance
+	 */
+	public float distance(SparseVector vector)
+	{
+		if (vector==null){
+			return this.length(false);
+		}
+		SparseVector newVector = minus(vector);
+		return newVector.length(false);
+	}
+	
+	
+	/**
 	 * 向量长度(模)的平方
-	 * @param updateCache 是否强制更新缓存
+	 * the square of vector length(norm of vector)
+	 * @param updateCache 是否强制更新缓存 - whether force to update cache
 	 * 如果这个参数为true，表示无论以前是否计算过，强制根据各个维度的值重新计算长度，并写入缓存
+	 * if true, then recalculate the length, and update cache
 	 * 如果这个参数为false，那么会根据是否计算过这个值，来决定从缓存中获取长度，还是根据维度的值来计算长度
-	 * @return 长度的平方
+	 * if false, then get value from cache when existing, otherwise recalculate
+	 * @return 长度的平方 - the square of vector length
 	 */
 	public float squareOfLength(boolean updateCache)
 	{
@@ -289,24 +342,64 @@ public class SparseVector {
 	}
 	
 	/**
-	 * 余弦相似性
-	 * 表达了两个向量夹角的大小
-	 * @return 夹角的余璇值
+	 * 向量长度(模)的平方
+	 * the square of vector length(norm of vector)
+	 * @return 长度的平方 - the square of vector length
 	 */
-	public float cosineSimilarity(SparseVector vector2)
+	public float squareOfLength()
 	{
-		if (vector2==null){
+		return squareOfLength(false);
+	}
+	
+	/**
+	 * 向量长度(模)
+	 * the vector length(norm of vector)
+	 * @param updateCache 是否强制更新缓存 - whether force to update cache
+	 * 如果这个参数为true，表示无论以前是否计算过，强制根据各个维度的值重新计算长度，并写入缓存
+	 * if true, then recalculate the length, and update cache
+	 * 如果这个参数为false，那么会根据是否计算过这个值，来决定从缓存中获取长度，还是根据维度的值来计算长度
+	 * if false, then get value from cache when existing, otherwise recalculate
+	 * @return 长度的平方 - vector length
+	 */
+	public float length(boolean updateCache)
+	{
+		return (float) Math.sqrt(squareOfLength(updateCache));
+	}
+	
+	/**
+	 * 向量长度(模)
+	 * the vector length(norm of vector)
+	 * @return 长度的平方 - vector length
+	 */
+	public float length()
+	{
+		return length(false);
+	}
+	
+	
+	/**
+	 * 余弦相似性
+	 * 表达了两个向量在方向上的接近程度，或者说夹角的大小
+	 * get cosine similarity, which show closing degree in direction
+	 * @param vector - another vector
+	 * @return 夹角的余弦值，余弦值的范围在[-1,1]之间，值越趋近于1，代表两个向量的方向越接近；越趋近于-1，他们的方向越相反；接近于0，表示两个向量近乎于正交；
+	 * cosine similarity [https://en.wikipedia.org/wiki/Cosine_similarity]
+	 */
+	public float cosineSimilarity(SparseVector vector)
+	{
+		if (vector==null){
 			throw new RuntimeException("0向量无法计算余弦相似性");
 		}
-		return dotProduct(vector2) / (squareOfLength(false) * vector2.squareOfLength(false));
+		return dotProduct(vector) / (squareOfLength(false) * vector.squareOfLength(false));
 	}
 	
 	
 	
 	/**
-	 * 求与当前向量(点)距离最近的向量(点)
-	 * @param vectors 一组要查找的向量
-	 * @return 最接近向量(点)的索引 (在vectors中的index)
+	 * 求与本向量(点)距离最近的向量(点)
+	 * giving a set of vectors, find minimal distance between this vector and one of them, return the index of minimal one 
+	 * @param vectors 一组要查找的向量 - a set of vectors
+	 * @return 最接近向量(点)的索引 (在vectors中的index) - the index of the minimal one 
 	 */
 	public int nearest(SparseVector[] vectors) {
 		float[] distances = new float[vectors.length];
@@ -318,6 +411,7 @@ public class SparseVector {
 	}
 	
 	//数组中最小值的索引
+	//the index of maximum value in the array 
 	private int minIndex(float[] array){
 		float minValue = array[0];
 		int index = 0;
@@ -332,6 +426,7 @@ public class SparseVector {
 	}
 
 	//数组中最大值的索引
+	//the index of maximum value in the array
 	private int maxIndex(float[] array){
 		float maxValue = array[0];
 		int index = 0;
@@ -346,10 +441,11 @@ public class SparseVector {
 	}
 	
 	/**
-	 * 求与当前向量(点)夹角最小的向量(点)
+	 * 求与本向量(点)夹角最小的向量(点)
 	 * 与nearest的差别在于, nearest求向量差最小,而本函数求向量夹角最小----也就是余弦相似性最大
-	 * @param vectors 要一一求夹角的向量组(如果其中有null值,直接忽略, 不会当做0向量处理)
-	 * @return 最靠近向量(点)的索引 (在vectors中的index)
+	 * giving a set of vectors, find minimal intersection angle between this vector and one of them, return the index of minimal one 
+	 * @param vectors 给定的向量组(如果其中有null值,直接忽略, 不会当做0向量处理) - a set of vectors(null item will be ignored) 
+	 * @return 最靠近向量(点)的索引 (在vectors中的index) - the index of minimal one 
 	 */
 	public int cloest(SparseVector[] vectors) {
 		float[] similarities = new float[vectors.length];
@@ -365,10 +461,11 @@ public class SparseVector {
 	}
 	
 	/**
-	 * 求与当前向量(点)点积最大向量(点)
+	 * 求与本向量(点)点积最大向量(点)
 	 * 与cloest的差别在于, cloest求向量夹角最小，此时两个向量有相同维度的贡献不够突出，而计算点积的时候，只要两个向量在相同维度上有值，就一定能获得一个比较大的乘积
-	 * @param vectors 要一一求点积的向量组(如果其中有null值,直接忽略, 不会当做0向量处理)
-	 * @return 最大点积(点)的索引 (在vectors中的index)
+	 * giving a set of vectors, find maximal dot product between this vector and one of them, return the index of maximal one 
+	 * @param vectors 要一一求点积的向量组(如果其中有null值,直接忽略, 不会当做0向量处理) - a set of vectors
+	 * @return 最大点积(点)的索引 (在vectors中的index) - the index of maximal one 
 	 */
 	public int maxDotProduction(SparseVector[] vectors) {
 		float[] productions = new float[vectors.length];
@@ -385,6 +482,7 @@ public class SparseVector {
 	
 	/**
 	 * 把维度名称和维度值序列化
+	 * serialized this vector, simply by serializing the map stores divisions
 	 */
 	public String toString()
 	{
@@ -393,15 +491,16 @@ public class SparseVector {
 	
 	/**
 	 * 寻找数值最大的几个维度值
-	 * @param count
-	 * @return
+	 * descending sort all divisions by value, and return top x of them 
+	 * @param x
+	 * @return 
 	 */
-	public Entry<String,Float>[] topDivisions(int count)
+	public Entry<String,Float>[] topDivisions(int x)
 	{
 		@SuppressWarnings("unchecked")
 		Entry<String,Float>[] set = divMap.entrySet().stream().sorted((Entry<String,Float> w1, Entry<String,Float> w2)->Float.compare(w2.getValue(),w1.getValue())).toArray(Entry[]::new);
-		if (count>=0 && count<set.length){
-			return Arrays.copyOfRange(set,0, count);
+		if (x>=0 && x<set.length){
+			return Arrays.copyOfRange(set,0, x);
 		}else {
 			return set;
 		}
@@ -409,12 +508,13 @@ public class SparseVector {
 	
 	/**
 	 * 寻找数值最大的几个维度的名称
-	 * @param count
+	 * descending sort all divisions by value, and return names of top x of them 
+	 * @param x
 	 * @return
 	 */
-	public String topDivString(int count)
+	public String topDivNames(int x)
 	{
-		Entry<String,Float>[] topEnties = topDivisions(count);
+		Entry<String,Float>[] topEnties = topDivisions(x);
 		StringBuilder builder= new StringBuilder("{");
 		for (int i=0; i<topEnties.length; i++){
 			Entry<String,Float> entry = topEnties[i];
@@ -431,9 +531,16 @@ public class SparseVector {
 	
 	/**
 	 * 把一组向量，按照给定的几个中心向量分类
+	 * 向量究竟离哪个中心最近，由ruleFunc决定
+	 * giving a set of vectors, split them into several groups surrounding given centers, use ruleFunc decide which center is closest to a specified vector
 	 * @param vectors 待分类的向量组
+	 * a set of vectors
 	 * @param centers 给定的向量中心
+	 * the centers for grouping
+	 * @param ruleFunc 需要传入一个双参数的方法，第一个参数是独立的vector，第二个参数是一组vector，返回一组vectors中，与独立vector关系最近的那条向量的索引
+	 * should be a function with 2 arguments, the first is a vector, the second is an array of vectors, and returns the index of closest one in array  
 	 * @return 分好类的向量组，外围List对应centers（分类中心的个数)
+	 * groups in outer list, and items in one group in inner list
 	 */
 	public static List<List<SparseVector>> aggregate(SparseVector[] vectors, SparseVector[] centers, BiFunction<SparseVector, SparseVector[],Integer> ruleFunc) {
 		
